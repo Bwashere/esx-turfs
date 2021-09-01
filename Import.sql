56d00b5e899b5f70ec257626beff95be6c50c66d
Brackzz <87747715+Bwashere@users.noreply.github.com>
2021-09-01 13:52:55 -0700

    Added Gang Sql

--- Import.sql
+++ Import.sql
@@ -1,1 +1,20 @@
--- Adding Later :pog:
+
+CREATE TABLE IF NOT EXISTS `gangs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `inventory` longtext DEFAULT NULL,
+  `leadership_rank` int(11) NOT NULL DEFAULT 3,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE IF NOT EXISTS `gang_ranks` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `label` varchar(255) DEFAULT NULL,
+  `gang_name` varchar(255) DEFAULT NULL,
+  `ranking` int(11) DEFAULT NULL,
+  `vehicles` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=86 DEFAULT CHARSET=latin1;
